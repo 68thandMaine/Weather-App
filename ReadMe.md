68469cf56598e78663d71f17af1b941df7379888
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-28 13:11:06 -0800

    Add readme

---
+++ ReadMe.md
@@ -0,0 +1,39 @@
+# Find Help!
+<sup>**Designed By:** _Chris Rudnicky_</sup>  
+<sup>_January 18, 2019_</sup>
+
+---
+
+## Table of Contents
+
+**[Description](#description)**<br>
+**[Installation Instructions](#installation-instructions)**<br>
+**[Application Specs](#application-specs)**<br>
+
+
+---
+## Description
+
+This is a JavaScript application that makes an API call to the Open Weather API.
+---
+## Installation Instructions
+
+To use this app:
+1. Clone the app from github using the following command:
+        $ git clone https://github.com/68thandMaine/weatherapp.git
+2. Run the following command to install the correct dependecies:
+        $ npm install
+3. You will need to create a file called .env which will hold our api key:
+        $ touch .env
+4. Obtain an API key from the OpenWeatherAPI and paste it into your .env file verbatium:
+        API_KEY= (paste your key here)
+5. Open the test environment to see use the app:
+        $ npm start
+
+---
+
+## Application specs
+
+No testing was used while creating this app.
+
+---
--- src/weather-interface.js
+++ src/weather-interface.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
-  $('weatherLocation').click(function(){
-    let city = $("location").val();
+  $('#weatherLocation').click(function(){
+    let today = new Date();
+    let city = $("#location").val();
     $("#location").val("");
-    $.ajax({
-      url: `http://api.openweathermap.org/data/2.5/weather?q=portland,us&appid=08d8f0feb4e2d502d633a314986f4813`
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=08d8f0feb4e2d502d633a314986f4813`;
 
-      type: `GET`,
-      data: {
-        format: `json`
-      },
-      success: function(response){
-        $(".showHumidity").text(`The humidity in${city} is ${response.main.humidity}%`);
-        $("showTemp").text(`The temperature in Kelivns is ${response.main.temp}%`);
-      },
-      error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.")
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
       }
-    });
+    }
+
+    request.open("Get", url, true);
+    request.send();
+
+    const getElements = function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`It will be ${response.main.temp} Kelvin today.`);
+      $('.showWeather').text(`The weather for ${today.toDateString()} calls for  ${response.weather[0].main}.`)
+    }
   });
 });
